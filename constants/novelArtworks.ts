c2ac102aa7a7a00a688ffd2108505895ef3eca94
sj.celinehong <sj.celinehong@gmail.com>
2026-05-03 06:50:10 +0900

    info add

--- constants/novelArtworks.ts
+++ constants/novelArtworks.ts
@@ -4,23 +4,23 @@ export const NOVEL_ARTWORKS: NonLoveArtPiece[] = [
   {
     id: 'n1',
     section: 'Novel',
-    title: '만화가가 마을을 떠났다.',
-    artist: 'MAGIO (@rizza____)',
-    subTitle: '로한과 죠스케가 사랑을 확인하는 내용을 쓰고 싶었습니다.',
+    title: 'Mille-Feuille and Fondant au chocolat',
+    artist: '베제타',
+    subTitle: '',
     story:
-      'g4Jk9fVqzT8bWmN2xR0hYpL1sEaU7cC5tZ6oQwX3vIiOeK4uBnPzYqLrS9mHjTfVbGxCkLqPp7wErT0yUiOpAsDfGhJkLzXcVbN7mQwErTyUiOpAsDfGhJkLmNzXcVbQwErTyUiOp',
+      '',
     thumbnail:
       'https://images.unsplash.com/photo-1481627834876-b7833e8f5570?auto=format&fit=crop&q=80&w=800',
   },
   {
     id: 'n2',
     section: 'Novel',
-    title: 'Midnight Chapter',
-    artist: 'Isobel Grey',
+    title: '가을의 중심에서 외치다',
+    artist: '한',
     subTitle:
-      'Moody blues and sharp whites depicting a desk illuminated only by a single candle.',
+      '',
     story:
-      'zP1xR9mN6sT4qW8bL0vC3hY2uK7aE5oF9gH2jS4dL6pR0tV8yQ1wX3nM5kZ7cB9vU2iO4eP6aS8dF0gH2jK4lQ9wErTyUiOpAsDfGhJkLmNzXcVbQwErTyUiOpZxCvBnM',
+      '',
     thumbnail:
       'https://images.unsplash.com/photo-1516414447565-b14be0adf13e?auto=format&fit=crop&q=80&w=800',
   },
